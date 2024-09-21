5f9938424ea368ee958ee92e6731b5e322c54565
Connor Rosewell <rosewcon@amazon.com>
2024-09-21 23:37:17 +0100

    Add resizing item cards depending on screen width

--- app/common_components/item_card_grid.tsx
+++ app/common_components/item_card_grid.tsx
@@ -1,16 +1,39 @@
+"use client";
+
 import { Grid } from "@cloudscape-design/components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ItemCard } from "./item_card";
 
 export const ItemCardGrid = ({items}: {items: any[] | undefined}) => {
+    const [colspan, setColspan] = useState(getColspan());
+
+    // Function to get colspan based on current window width
+    function getColspan() {
+        if (typeof window !== 'undefined') {
+            if (window.innerWidth < 810) {
+                return 12;
+            } else if (window.innerWidth < 1235) {
+                return 6;
+            }
+        }
+        return 4;
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            setColspan(getColspan());
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    const gridDefinition = items?.map(() => ({ colspan }));
     return (
         <div className="items_grid">
             <Grid
-
-                gridDefinition={[
-                    { colspan: 4 },
-                    { colspan: 4 },
-                ]}
+                gridDefinition={gridDefinition}
             >
                 {items?.map(
                     (item) => (
